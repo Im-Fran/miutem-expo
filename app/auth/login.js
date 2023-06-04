8ee680152e4f3bd43d8dc1aad0887f48f0e22271
Francisco Solis <imfran@duck.com>
2023-06-04 10:36:20 -0400

    WIP: Login

--- app/auth/login.js
+++ app/auth/login.js
@@ -1,3 +1,24 @@
-export default function Login({ }) {
+import {StyleSheet, Text, View} from "react-native";
+import useStoredState from "../../src/storage/StoredState";
+import {getStyles} from "../../src/utils/Styling";
 
+export default function Login({ }) {
+    const [darkMode, setDarkMode] = useStoredState("darkMode", false)
+
+    return <View style={getStyles(darkMode).container}>
+        <Text style={styles.text}>
+            Hello, Login!
+        </Text>
+    </View>
 }
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: 'black',
+    },
+})
--- app/index.js
+++ app/index.js
@@ -5,14 +5,23 @@ import { getStyles } from "../src/utils/Styling";
 import {Button, Text, View} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
 import Splash from "./splash";
+import {useRouter} from "expo-router";
 
 export default function App() {
+    const router = useRouter()
     const [isReady, setReady] = useState(false)
     const [darkMode, setDarkMode] = useStoredState("darkMode", false)
 
     useEffect(() => {
         setReady(false)
     }, [])
+
+    useEffect(() => {
+        if(isReady) {
+            // Redirect to '/login'
+            router.push('/auth/login')
+        }
+    }, [isReady]);
 
     return isReady ? <View style={getStyles(darkMode).container}>
         <Text style={getStyles(darkMode).text} onPress={() => setDarkMode(!darkMode)}>Hello, World!</Text>
