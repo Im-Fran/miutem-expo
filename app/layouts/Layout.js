b1e21ef5bf20b66433828dc5735a888147b8c2f2
Francisco Solis <imfran@duck.com>
2023-06-07 15:56:57 -0400

    Improvements

--- app/layouts/Layout.js
+++ app/layouts/Layout.js
@@ -1,16 +1,16 @@
 import {StatusBar} from "expo-status-bar";
 import {SafeAreaView, useColorScheme, View} from "react-native";
-import { ToastProvider } from 'react-native-toast-notifications'
+import {ToastProvider} from 'react-native-toast-notifications'
 
 export default function Layout({ children, className = '', statusBarStyle = null, hideStatusBar = false }) {
     const { darkMode } = useColorScheme();
 
-    return <>
-        <ToastProvider>
-            <SafeAreaView className={`flex-1 items-center justify-center ${className}`}>
-                {children}
-            </SafeAreaView>
-        </ToastProvider>
+    return <ToastProvider
+        placement="bottom"
+    >
+        <SafeAreaView className={`flex-1 items-center justify-center ${className}`}>
+            {children}
+        </SafeAreaView>
         <StatusBar style={statusBarStyle || (darkMode ? 'light' : 'dark')} hidden={hideStatusBar}/>
-    </>
+    </ToastProvider>
 }
